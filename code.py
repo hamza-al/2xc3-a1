99e635ff19ff75ed8755bc773c6cc3119c60a0c8
Hamza Mohammad <alsorkhyy@gmail.com>
2022-01-18 16:49:02 -0500

    added conditions, made str numbers

--- code.py
+++ code.py
@@ -1,4 +1,10 @@
-def are_valid_groups(numbers, groups):
+from typing import List
+
+
+def are_valid_groups(numbers: List[str], groups: List[List[str]]):
+    for group in groups:
+        if len(group) > 3 or len(group) < 2:
+            return False
     students_in_groups = []
     for group in groups:
         for student in group:
